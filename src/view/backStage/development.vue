1373ec337695300fe491a7017f1daa27e39ae4ea
chenyuelin <cyl1095367148@163.com>
2022-06-22 18:11:55 +0800

    头像上传

---
+++ src/view/backStage/development.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ctab :breadcrumb="breadcrumb"></ctab>
+    
+  </div>
+</template>
+
+<script>
+import ctab from '../../components/ctab.vue'
+export default {
+  components: { ctab },
+  name:'development',
+  data() {
+    return {
+      breadcrumb:[{name:'开发管理',path:''}],
+    }
+  },
+  mounted(){
+
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+}
+
+</script>
+<style scoped lang='scss'>
+
+</style>
